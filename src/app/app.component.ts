474f7f9263ec1412b5b4c37075055550653a8f3b
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2023-12-13 21:26:46 +0100

    padre-hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'redux-app';
+
+  contador: number;
+
+  constructor() {
+    this.contador = 0;
+  }
+
+  incrementar() {
+    this.contador += 1;
+  }
+
+  decrementar() {
+    this.contador -= 1;
+  }
+
+  restablecer() {
+    this.contador = 0;
+  }
+
+
 }
---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html'
+})
+export class HijoComponent implements OnInit {
+  
+  @Input()
+  contador: number = 0;
+
+  @Output() cambioContador = new EventEmitter<number>();
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  multiplicar() {
+    this.contador *= 2;
+    this.cambioContador.emit(this.contador);
+  }
+
+  dividir() {
+    this.contador /= 2;
+    this.cambioContador.emit(this.contador);
+  }
+
+  restablecer() {
+    this.contador = 0;
+    this.cambioContador.emit(this.contador);
+  }
+}
